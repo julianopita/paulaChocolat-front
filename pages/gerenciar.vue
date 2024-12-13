5814772c0b0b1c68e0c601e35becee4ef8b670ff
Rafael Capodeferro <rafael222.rc@gmail.com>
2024-12-13 16:22:25 -0300

    style: adiciona novo CSS para estilizar formulários

--- pages/gerenciar.vue
+++ pages/gerenciar.vue
@@ -143,33 +143,41 @@ onMounted(carregarProdutos);
 
 <template>
   <main>
-    <h1>Gerenciar Produtos</h1>
+    <h1 style="text-align: center;">Gerenciar Produtos</h1>
 
     <!-- Exibição de mensagens de erro e sucesso -->
     <div v-if="mensagemErro" class="erro">{{ mensagemErro }}</div>
     <div v-if="mensagemSucesso" class="sucesso">{{ mensagemSucesso }}</div>
 
     <!-- Lista de produtos -->
+    
     <section>
-      <h2>Produtos Atuais</h2>
-      <ul>
-        <li v-for="produto in produtos" :key="produto.id">
-          <div>
-            <h3>{{ produto.titulo }}</h3>
-            <p>{{ produto.descricao }}</p>
-            <p>Preço: R$ {{ produto.preco.toFixed(2) }}</p>
-            <img :src="`${imageEndpoint}/${produto.imagem}`" alt="Imagem do produto" width="100" />
-            <button @click="deletarProduto(produto.id)">Excluir</button>
-            <button @click="abrirFormularioEdicao(produto)">Editar</button>
+      <div class="dishes">
+        <ul class="card-container">
+        <li v-for="produto in produtos" :key="produto.id" style="list-style: none;">
+          <div class="card">
+            <img :src="`${imageEndpoint}/${produto.imagem}`" class="card-image" :alt="produto.titulo" width="150px"/>
+            <h3 class="card-title">{{ produto.titulo }}</h3>
+            <span class="card-description">{{ produto.descricao }}</span>
+            <div class="card-price">
+              <h4>R$ {{ produto.preco.toFixed(2).replace('.', ',') }}</h4>
+            </div>
+            <div>
+              <button style="background-color: #ff9900;" class="card-button" @click="abrirFormularioEdicao(produto)">Editar</button>
+              <button style="background-color: #c50d0d;" class="card-button" @click="deletarProduto(produto.id)">Excluir</button>
+            </div>
           </div>
         </li>
       </ul>
+      </div>
+      <!-- <h2>Produtos Atuais</h2> -->
+      
     </section>
 
     <!-- Formulário de edição ou criação -->
-    <section v-if="produtoEdicao">
+    <section class="section-gerenciar-form" v-if="produtoEdicao">
       <h2>{{ criandoNovoProduto ? 'Criar Produto' : 'Editar Produto' }}</h2>
-      <form @submit.prevent="criandoNovoProduto ? criarProduto() : atualizarProduto()">
+      <form class="form-criar-produto" @submit.prevent="criandoNovoProduto ? criarProduto() : atualizarProduto()">
         <label for="titulo">Título</label>
         <input v-model="produtoEdicao.titulo" type="text" id="titulo" required />
 
@@ -177,33 +185,118 @@ onMounted(carregarProdutos);
         <textarea v-model="produtoEdicao.descricao" id="descricao" required></textarea>
 
         <label for="preco">Preço</label>
-        <input v-model.number="produtoEdicao.preco" type="number" id="preco" required />
+        <input v-model.number="produtoEdicao.preco" type="number" step="0.01" id="preco" required />
 
         <label for="imagem">Imagem</label>
         <input type="file" @change="handleFileChange" id="imagem" />
-
-        <button type="submit">{{ criandoNovoProduto ? 'Criar Produto' : 'Salvar Alterações' }}</button>
-        <button type="button" @click="produtoEdicao = null; criandoNovoProduto = false;">Cancelar</button>
+        <div class="form-button-container">
+          <button class="form-button" type="submit">{{ criandoNovoProduto ? 'Criar Produto' : 'Salvar Alterações' }}</button>
+          <button class="form-button" type="button" @click="produtoEdicao = null; criandoNovoProduto = false;">Cancelar</button>
+        </div>
       </form>
     </section>
 
     <!-- Botão para criar novo produto -->
-    <section>
-      <button @click="abrirFormularioCriacao">Criar Novo Produto</button>
+    <section style="display: flex; justify-content: center;">
+      <button @click="abrirFormularioCriacao" class="form-button">Criar Novo Produto</button>
     </section>
   </main>
 </template>
 
+<style src="../assets/css/produtos_componente.css"></style>
+<style src="../assets/css/formularios.css"></style>
+
 <style>
+/* .section-gerenciar-form{
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form-criar-produto{
+  background-color: #ffffff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  max-width: 500px;
+}
+
+.form-button-container{
+  display: flex;
+  justify-content: center
+}
+
+label {
+    display: block;
+    margin-bottom: 8px;
+    font-weight: bold;
+    color: #555;
+}
+
+input[type="text"],
+input[type="number"],
+textarea{
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    font-size: 14px;
+    background-color: #f9f9f9;
+}
+
+textarea {
+    resize: vertical;
+}
+
+input[type="file"] {
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  display: inline-block;
+  padding: 6px 12px;
+  cursor: pointer;
+}
+
+input[type="file"]::file-selector-button{
+  font-weight: bold;
+  color: dodgerblue;
+  padding: 0.5em;
+  margin-right: 10px;
+  border: thin solid grey;
+  border-radius: 3px;
+}
+
+input[type="file"]::file-selector-button:hover{
+  cursor: pointer;
+}
+
+.form-button {
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  font-size: 14px;
+  cursor: pointer;
+  color: #fff;
+  background-color: #007bff;
+  margin-right: 10px;
+  transition: background-color 0.3s;
+}
+
+.form-button:hover {
+    background-color: #0056b3;
+} */
+
 .erro {
   color: red;
 }
 
 .sucesso {
   color: green;
-}
-
-button {
-  margin: 5px;
 }
 </style>
