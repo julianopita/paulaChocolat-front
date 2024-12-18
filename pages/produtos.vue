1d9b240e405fab3c5b91c3942932c101fbdc5018
Rafael Capodeferro <rafael222.rc@gmail.com>
2024-12-18 16:46:07 -0300

    style: corrige erros no estilo dos produtos

--- pages/produtos.vue
+++ pages/produtos.vue
@@ -3,7 +3,6 @@ import Products from '~/components/Products.vue'; // Importação correta
 </script>
 
 <template>
-    Produtos dinâmicos
  <Products apiEndpoint="http://localhost:3000/produtos" imageEndpoint="http://localhost:3000/static"/>
 </template>
 
